aca4b5569787d80258b144eb130fa3465cf316df
MariaLin <becauseyu520@gmail.com>
2025-01-22 13:10:33 +0800

    20250122 測試動態改 scripts

--- data/GAS.js
+++ data/GAS.js
@@ -1,8 +1,8 @@
-var gas = "https://script.google.com/macros/s/AKfycbxkS21rC1mPLbmSUr8DCSYz0-lyX_kvJcIM-9PH-noaENZXg89faKjzmc4qIZtiHLbVwA/exec";
 // 20250121 Maria 用於取得ㄓ用於取得 試算表的資料
 function GetBirthDay (){
     var data ;
     $.ajax({
+        // 因為我用git 去通過驗證，所以必須給 token 讓他通過
         "headers": {authorization: "Bearer AKfycbwMdriL5N0SjN7cECcyXJn9yvRe5MKA3kyYxZOaOw-U"},
         async: false,
         type: "post",
@@ -11,7 +11,7 @@ function GetBirthDay (){
             "sheetName": "生日",
         },
         // 填入網路應用程式網址
-        url: gas,
+        url: GAS_URL,
         success:
             function (result) {
                 data =  result;
--- public/main/main.js
+++ public/main/main.js
@@ -1,4 +1,16 @@
-$(function(){
-    var data = GetBirthDay();
-    console.log(data);
-})
+// 先設定路由的全域變數
+if ( location.href.includes("github")){
+  // 取得所有的 <script> 元件
+    var scripts = document.querySelectorAll('script');
+    // 遍歷每個 <script> 元件
+    scripts.forEach(function(script) {
+        // 確保 src 存在且不是空字串
+        if (script.src) {
+            // 將 src 修改為原本的 src 加上 "aaa/"
+            var originalSrc = script.getAttribute('src');
+            var newSrc = originalSrc + '/takoiscute/';
+            script.setAttribute('src', newSrc);
+        }
+    });
+}
+
