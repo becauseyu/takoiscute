79d8a24421411fb3b68327226e0ff4f4474439b4
MariaLin <becauseyu520@gmail.com>
2025-01-26 11:06:56 +0800

    20250126 更新基礎模板

--- data/GAS.js
+++ data/GAS.js
@@ -1,5 +1,5 @@
 // 20250121 Maria 用於取得ㄓ用於取得 試算表的資料
-function GetBirthDay (){
+function GetGasData (sheetName,para = {}){
     var data ;
     $.ajax({
         // 因為我用git 去通過驗證，所以必須給 token 讓他通過
@@ -8,13 +8,34 @@ function GetBirthDay (){
         type: "post",
         data: {
             "method": "read",
-            "sheetName": "生日",
+            "sheetName": sheetName,
         },
         // 填入網路應用程式網址
         url: GAS_URL,
         success:
             function (result) {
-                data =  result;
+                data =  result;    
+            }
+    });
+    return data;
+}
+
+function InsertGasData (sheetName,para = {}){
+    var data ;
+    $.ajax({
+        // 因為我用git 去通過驗證，所以必須給 token 讓他通過
+        "headers": {authorization: "Bearer AKfycbwMdriL5N0SjN7cECcyXJn9yvRe5MKA3kyYxZOaOw-U"},
+        async: false,
+        type: "post",
+        data: {
+            "method": "write",
+            "sheetName": sheetName,
+        },
+        // 填入網路應用程式網址
+        url: GAS_URL,
+        success:
+            function (result) {
+                data =  result;    
             }
     });
     return data;
--- public/main/main.js
+++ public/main/main.js
@@ -1,4 +1,4 @@
-// 先設定路由的全域變數
+/*動態設定路由*/
 if ( location.href.includes("github")){
   // 取得所有的 <script> 元件
     var scripts = document.querySelectorAll('script');
@@ -10,6 +10,17 @@ if ( location.href.includes("github")){
             var originalSrc = script.getAttribute('src');
             var newSrc = originalSrc + '/takoiscute/';
             script.setAttribute('src', newSrc);
+        }
+    });
+    var links = document.querySelectorAll('link');
+    // 遍歷每個 <link> 元件
+    links.forEach(function(link) {
+        // 確保 src 存在且不是空字串
+        if (link.src) {
+            // 將 src 修改為原本的 src 加上 "aaa/"
+            var originalHref = link.getAttribute('href');
+            var newHref = originalHref + '/takoiscute/';
+            link.setAttribute('src', newHref);
         }
     });
 }
