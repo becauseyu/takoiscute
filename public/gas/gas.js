c650649e5cf1b46ba8689fe426b35c68b517d72a
Maria Lin <becauseyu520@gmail.com>
2025-01-31 23:53:15 +0800

    20250131 效果測試

--- public/gas/gas.js
+++ public/gas/gas.js
@@ -1,18 +1,14 @@
 // 20250121 Maria 用於取得用於取得 試算表的資料
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxIZezO5otCSxIaasRMO81nCXpOR_zBWTDr0m33KxKyh7kBmlgs4qxGX8RT-LGkosBa4w/exec";
-const GAS_token = "Bearer " + "AKfycbwMdriL5N0SjN7cECcyXJn9yvRe5MKA3kyYxZOaOw-U"
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwmqnoFT-DrDCuuLNMMU-iBnw0ila0HowpaPdy46AHeq1nBfybXoGC12aTJAiVZoaLgAA/exec";
 function GetGasData (sheetName,para = {}){
     var data ;
     $.ajax({
-        // 因為我用git 去通過驗證，所以必須給 token 讓他通過
-        // headers: {
-        //     "Authorization": GAS_token,
-        // },
         async: false,
         type: "POST",
         data: {
             "method": "read",
             "sheetName": sheetName,
+            'other_para':para,
         },
         // 填入網路應用程式網址
         url: GAS_URL,
@@ -27,13 +23,11 @@ function GetGasData (sheetName,para = {}){
 function InsertGasData (sheetName,para = {}){
     var data ;
     $.ajax({
-        // 因為我用git 去通過驗證，所以必須給 token 讓他通過
-        headers: {authorization: GAS_token},
-        async: false,
         type: "post",
         data: {
             "method": "write",
             "sheetName": sheetName,
+            'other_para':para,
         },
         // 填入網路應用程式網址
         url: GAS_URL,
