81dadb2edc33e1625a9d5a8fc62eadd7c10fb5df
Maria Lin <becauseyu520@gmail.com>
2025-01-31 22:42:14 +0800

    20250131 gas 測試

--- public/gas/gas.js
+++ public/gas/gas.js
@@ -1,17 +1,15 @@
-// 20250121 Maria 用於取得ㄓ用於取得 試算表的資料
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxkS21rC1mPLbmSUr8DCSYz0-lyX_kvJcIM-9PH-noaENZXg89faKjzmc4qIZtiHLbVwA/exec";
+// 20250121 Maria 用於取得用於取得 試算表的資料
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxIZezO5otCSxIaasRMO81nCXpOR_zBWTDr0m33KxKyh7kBmlgs4qxGX8RT-LGkosBa4w/exec";
 const GAS_token = "Bearer " + "AKfycbwMdriL5N0SjN7cECcyXJn9yvRe5MKA3kyYxZOaOw-U"
 function GetGasData (sheetName,para = {}){
     var data ;
     $.ajax({
         // 因為我用git 去通過驗證，所以必須給 token 讓他通過
-        headers: {
-            "Access-Control-Allow-Origin":'*',
-            "authorization": GAS_token,
-            "Content-Type": 'applicaion/json',
-        },
+        // headers: {
+        //     "Authorization": GAS_token,
+        // },
         async: false,
-        type: "post",
+        type: "POST",
         data: {
             "method": "read",
             "sheetName": sheetName,
