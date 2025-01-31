09be7901b9c5c3ce00f3f30012da39f3f88f6441
MariaLin <107749881+becauseyu@users.noreply.github.com>
2025-01-31 21:48:03 +0800

    Update gas.js

--- public/gas/gas.js
+++ public/gas/gas.js
@@ -1,9 +1,11 @@
 // 20250121 Maria 用於取得ㄓ用於取得 試算表的資料
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxkS21rC1mPLbmSUr8DCSYz0-lyX_kvJcIM-9PH-noaENZXg89faKjzmc4qIZtiHLbVwA/exec";
+const GAS_token = "Bearer" + "AKfycbwMdriL5N0SjN7cECcyXJn9yvRe5MKA3kyYxZOaOw-U"
 function GetGasData (sheetName,para = {}){
     var data ;
     $.ajax({
         // 因為我用git 去通過驗證，所以必須給 token 讓他通過
-        "headers": {authorization: "Bearer AKfycbwMdriL5N0SjN7cECcyXJn9yvRe5MKA3kyYxZOaOw-U"},
+        headers: {authorization: token},
         async: false,
         type: "post",
         data: {
@@ -24,7 +26,7 @@ function InsertGasData (sheetName,para = {}){
     var data ;
     $.ajax({
         // 因為我用git 去通過驗證，所以必須給 token 讓他通過
-        "headers": {authorization: "Bearer AKfycbwMdriL5N0SjN7cECcyXJn9yvRe5MKA3kyYxZOaOw-U"},
+        headers: {authorization: token},
         async: false,
         type: "post",
         data: {
